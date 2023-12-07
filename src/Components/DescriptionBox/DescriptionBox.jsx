dae7db933cdd52afa6564db166aeeeb10d14c15e
Giga Basharuli <basharuligiga@gmail.com>
2023-12-07 20:08:18 +0400

    done

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (133)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          aliquid praesentium dolores, minima dolor, corporis consequuntur
+          doloribus saepe repellat velit dolore facere nemo iure molestiae
+          reprehenderit illo nulla, laudantium sed mollitia quasi nisi id.
+          Voluptates modi provident a iste recusandae molestiae culpa. Eius
+          labore nobis alias facere incidunt hic perspiciatis ex cumque
+          praesentium, sit eaque ut ullam iste voluptatum nemo.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+          magni. In cupiditate eligendi sint asperiores ratione porro maxime
+          dolorem explicabo magnam ullam, alias rem eveniet, quis veniam harum
+          pariatur consectetur esse atque!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
